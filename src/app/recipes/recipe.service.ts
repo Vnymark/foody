2d9492d4dad03ee21b7d3336714ea0401b8a1fc0
Vnymark <vnymark@gmail.com>
2017-10-06 00:49:53 +0200

    Added ingredients to recipes.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,29 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'Testing', 'http://alturl.com/6wiwp'),
-    new Recipe('Another Test Recipe', 'Testing', 'http://alturl.com/6wiwp')
+    new Recipe(
+      'Schnitzel',
+      'Schnitzel, served with french fries',
+      'https://images.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.foodworks.com%2Fuploads%2Ftx_sschproductbase%2Fschweineschnitzel.png&f=1',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe(
+      'Hamburger',
+      'A simple hamburger with cheese.',
+      'https://images.duckduckgo.com/iu/?u=http%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F4%2F4d%2FCheeseburger.jpg&f=1',
+      [
+        new Ingredient('Bread', 2),
+        new Ingredient('Meat', 1),
+        new Ingredient('Slice of cheese', 1),
+        new Ingredient('Lettuce', 1)
+      ])
   ];
 
   getRecipes() {
