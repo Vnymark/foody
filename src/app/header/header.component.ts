cfb2f1d1e5602473290b7ccf950daf696e724230
Vnymark <vnymark@gmail.com>
2017-10-06 01:24:54 +0200

    Added routing and changed the navigation to use routes instead of click-events.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,11 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent{
-  @Output() menuChoice = new EventEmitter<string>();
+export class HeaderComponent {
 
-  onSelect(choice: string) {
-    this.menuChoice.emit(choice);
-
-  }
 
 }
