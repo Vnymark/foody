0accf5e04a1ec898396d477430e6a40dbf38eaa1
Vnymark <vnymark@gmail.com>
2017-10-06 00:14:03 +0200

    Cleaned the code and used the new service for cross component communication instead.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
 import { Recipe } from './recipe.model';
+import { EventEmitter } from '@angular/core';
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'Testing', 'http://alturl.com/6wiwp'),
     new Recipe('Another Test Recipe', 'Testing', 'http://alturl.com/6wiwp')
