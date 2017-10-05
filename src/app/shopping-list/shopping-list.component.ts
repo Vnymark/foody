042ae0baa8b931b96fd3ad125279e0d7212a76c7
Vnymark <vnymark@gmail.com>
2017-10-05 21:53:42 +0200

    Added the functionality to add ingredients to the shopping-list.

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 import { Ingredient } from '../shared/ingredient.model';
 
@@ -7,15 +7,14 @@ import { Ingredient } from '../shared/ingredient.model';
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent {
   ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10)
   ];
 
-  constructor() { }
-
-  ngOnInit() {
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
   }
 
 }
